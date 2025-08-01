0e93d2c069b8ac2428e98ed53e05f3c14fca3506
Sekaiarchivist <sekai.archivist@gmail.com>
2025-08-01 23:23:10 +0800

    Create the GazettE - REGRET [PAL DVD 480p PCM MPEG-2 REMUX-SEKAI].mk

---
+++ the GazettE/the GazettE - REGRET [PAL DVD 480p PCM MPEG-2 REMUX-SEKAI].mk
@@ -0,0 +1,75 @@
+General
+Unique ID                   : 76515597852526745158862668072374337033 (0x39905E3C914A33B4DF8453DAEA497E09)
+Complete name               : the GazettE - REGRET [PAL DVD 480p PCM MPEG-2 REMUX-SEKAI].mkv
+Format                      : Matroska
+Format version              : Version 4
+File size                   : 323 MiB
+Duration                    : 4 min 43 s
+Overall bit rate mode       : Variable
+Overall bit rate            : 9 553 kb/s
+Frame rate                  : 29.970 FPS
+Movie name                  : the GazettE - REGRET [PAL DVD 480p PCM MPEG-2 REMUX-SEKAI]
+Encoded date                : 2025-07-31 21:10:30 UTC
+Writing application         : mkvmerge v94.0 ('Initiate') 64-bit
+Writing library             : libebml v1.4.5 + libmatroska v1.7.1
+Website                     : https://sekai-archivist.blogspot.com/
+
+Video
+ID                          : 1
+ID in the original source m : 224 (0xE0)
+Format                      : MPEG Video
+Format version              : Version 2
+Format profile              : Main@Main
+Format settings             : CustomMatrix / BVOP
+Format settings, BVOP       : Yes
+Format settings, Matrix     : Custom
+Format settings, GOP        : Variable
+Format settings, picture st : Frame
+Codec ID                    : V_MPEG2
+Codec ID/Info               : MPEG 1 or 2 Video
+Duration                    : 4 min 43 s
+Bit rate mode               : Variable
+Bit rate                    : 8 014 kb/s
+Maximum bit rate            : 9 800 kb/s
+Width                       : 720 pixels
+Height                      : 480 pixels
+Display aspect ratio        : 4:3
+Frame rate mode             : Constant
+Frame rate                  : 29.970 (30000/1001) FPS
+Standard                    : NTSC
+Color space                 : YUV
+Chroma subsampling          : 4:2:0
+Bit depth                   : 8 bits
+Scan type                   : Interlaced
+Scan order                  : Top Field First
+Compression mode            : Lossy
+Bits/(Pixel*Frame)          : 0.774
+Time code of first frame    : 01:00:00:00
+Time code source            : Group of pictures header
+GOP, Open/Closed            : Open
+GOP, Open/Closed of first f : Closed
+Stream size                 : 271 MiB (84%)
+Language                    : Japanese
+Default                     : Yes
+Forced                      : No
+Original source medium      : DVD-Video
+
+Audio
+ID                          : 2
+ID in the original source m : 189 (0xBD)160 (0xA0)
+Format                      : PCM
+Format settings             : Little / Signed
+Codec ID                    : A_PCM/INT/LIT
+Duration                    : 4 min 43 s
+Bit rate mode               : Constant
+Bit rate                    : 1 536 kb/s
+Channel(s)                  : 2 channels
+Sampling rate               : 48.0 kHz
+Frame rate                  : 30.000 FPS (1600 SPF)
+Bit depth                   : 16 bits
+Stream size                 : 52.0 MiB (16%)
+Language                    : Japanese
+Default                     : Yes
+Forced                      : No
+Original source medium      : DVD-Video
+
